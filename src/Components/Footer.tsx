1d4fc8f421d33b3b67094edc1f6f9b18273fce55
ishikamanchanda30 <ishikamanchanda0130@gmail.com>
2025-03-27 16:49:53 +0530

    made footer responsive

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,23 +1,23 @@
 function Footer() {
   return (
-    <>
-      <div className='flex flex-col justify-between bg-[#0D0D0D] text-white overflow-hidden' >
-        <div className='mx-auto my-0 text-[8rem] text-center p-0'>
+    
+      <div className='max-w-screen flex flex-col justify-between bg-[#0D0D0D] text-white overflow-hidden' >
+        <div className='mx-auto my-0  md:text-[4rem] lg:text-[8rem] text-left md:text-center p-0'>
           DIGITAL ARTS SOCIETY
         </div>
-        <div className='flex h-[1px] bg-white/70  min-w-screen'>
+        <div className='flex h-[1px] bg-white/70 min-w-screen'>
         </div>
-        <div className='flex flex-row justify-between px-10 text-white py-2'>
+        <div className='flex flex-col  md:flex-row text-center md:justify-between px-10 text-white py-2'>
           <div className='text-[#878787]'>
             <div>
               &#169;2025 All Rights Reserved
             </div>
           </div>
-          <div className='flex flex-row justify-between gap-4 cursor-pointer '>
-            <a href="/contact" className='border-r-2 border-white pr-4'>
+          <div className='flex flex-col md:flex-row justify-between gap-4 cursor-pointer text-center '>
+            <a href="/contact" className='md:border-r-2 border-white md:pr-4'>
               Contact Us
             </a>
-            <a href="/cookies" className='border-r-2 border-white pr-4'>
+            <a href="/cookies" className='md:border-r-2 border-white md:pr-4'>
               Cookie Policy
             </a>
             <a href="/" >
@@ -26,7 +26,7 @@ function Footer() {
           </div>
         </div>
       </div>
-    </>
+    
   )
 }
 
--- src/Pages/Cookies.tsx
+++ src/Pages/Cookies.tsx
@@ -2,8 +2,8 @@ import Cookie from '../Components/Cookie'
 function Cookies() {
   return (
     <>
-      <div className='flex flex-col justify-center items-center bg-[#0D0D0D] text-white min-w-screen min-h-screen py-20 gap-2'>
-        <div className='text-2xl font-bold'>
+      <div className='flex flex-col justify-center items-center bg-[#0D0D0D] text-white text-center min-w-screen min-h-screen py-20 gap-2'>
+        <div className='text-xl font-bold'>
           Hi, you've been here for a while. Have a cookie.
         </div>
         <div>
