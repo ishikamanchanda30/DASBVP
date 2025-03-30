ddcd333471b38fe570a815e923faa67fd34d523c
ishikamanchanda30 <ishikamanchanda0130@gmail.com>
2025-03-31 02:44:51 +0530

    Made Contact & Flagship Pages responsive

--- src/Pages/FlagshipEvent.tsx
+++ src/Pages/FlagshipEvent.tsx
@@ -16,21 +16,21 @@ const FlagshipEvent: React.FC = () => {
   }]
   return (
     <div className='w-screen h-screen bg-[#0D0D0D] text-white font-kumbh flex flex-col justify-center items-center'>
-      <div className='h-[40%] flex flex-col justify-start items-start w-[95%]'>
-        <span className='text-9xl font-bold'>FLAGSHIP</span>
-        <span className='text-6xl font-semibold'>{date}</span>
+      <div className='h-[20%] mt-18 md:mt-0 md:h-[40%]  flex flex-col justify-start items-start w-[95%]'>
+        <span className='text-7xl md:text-9xl font-bold'>FLAGSHIP</span>
+        <span className='text-6xl md:text-6xl font-semibold'>{date}</span>
       </div>
-      <div className="h-[40%] flex w-[95%] gap-8"> {/* Parent container */}
+      <div className="h-[80%] md:h-[40%] p-5 md:p-0 flex flex-col md:flex-row w-[95%] gap-8"> {/* Parent container */}
         {events.map((event, index) => (
           <div
             key={index}
-            className="h-[90%] border rounded-xl flex flex-col justify-evenly items-center 
+            className="md:h-[90%]  border rounded-xl flex flex-col justify-evenly items-center 
                       transition-all duration-300 flex-1 hover:flex-[1.4]" 
             /* flex-1 = equal width, hover:flex-[1.2] = grows 20% larger */
           >
             <div className="w-[90%] h-[10%] flex justify-between items-center">
               <div className="h-full aspect-square bg-white rounded-full" />
-              <span className="text-lg hover:underline cursor-pointer">REGISTER</span>
+              <span className="text-lg hover:underline cursor-pointer"><a href="">REGISTER</a></span>
             </div>
             <div className="w-[90%] h-[50%] flex flex-col justify-center">
               <span className="text-2xl">{event.name}</span>
