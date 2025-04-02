14f81adc53d03b768c036744cab64ff53afd56d0
Harshit1607 <hbareja.07@gmail.com>
2025-04-02 12:31:33 +0530

    heads page

--- src/Components/HeadCard.tsx
+++ src/Components/HeadCard.tsx
@@ -55,18 +55,15 @@ const HeadCard = () => {
         },
     ]
     return (
-        <div className="flex flex-row w-full justify-center items-center text-gray-300  ">
-            <div className="flex flex-wrap justify-center items-center gap-x-20 gap-y-10 w-full md:w-4/5">
+        
+            <div className="flex flex-wrap justify-between items-center gap-x-20 gap-y-10 w-full md:w-4/5 h-[80%] w-full">
                 {heads.map((head, index) => (
-                    <div key={index} className="relative h-90 w-4/5 overflow-hidden rounded-xl md:w-1/5">
-                        <div className="absolute inset-0 "></div>
-                        <div className="absolute inset-0">
-                            <img
-                                src={head.image}
-                                alt={head.name}
-                                className="w-full h-full object-cover grayscale"
-                            />
-                        </div>
+                    <div key={index} className="relative h-[45%] w-4/5 overflow-hidden rounded-xl md:w-1/5">
+                        <img
+                            src={head.image}
+                            alt={head.name}
+                            className="w-full h-full object-cover grayscale"
+                        />
                         <div className="absolute bottom-0 left-0 p-5 z-20 ">
                             <div className="text-2xl font-bold text-white ">{head.name}</div>
                             <div className="text-sm text-gray-300">{head.position}</div>
@@ -74,7 +71,6 @@ const HeadCard = () => {
                     </div>
                 ))}
             </div>
-        </div>
     )
 }
 
--- src/Components/Heads.tsx
+++ src/Components/Heads.tsx
@@ -3,8 +3,8 @@ import HeadCard from './HeadCard'
 const Heads: React.FC = () => {
 
   return (
-    <div className="text-white w-full h-full flex flex-col pt-15 items-center ">
-      <div className="flex flex-row w-[90%] justify-between items-center text-[#D3D3D3] text-7xl pb-10">
+    <div className="text-white w-full h-full flex flex-col pt-15 items-center">
+      <div className="flex flex-row w-[85%] justify-between items-center text-[#D3D3D3] text-6xl pb-10 h-[15%]">
         <span className='pr-4'>HEADS </span>
         <span className='flex w-[90%] border-1 border-[#D3D3D3]'></span>
         <span className='pl-4'>DAS</span>
