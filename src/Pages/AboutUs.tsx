981f0d145dda420b9f58c48f89a7551c0a1bef18
teena14 <teena14095@gmail.com>
2025-07-12 18:27:33 +0530

    removed core from about us due to responsiveness problem

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -1,13 +1,11 @@
 import Faculty from '../Components/Faculty';
 import React from 'react';
 import Home from '../Components/Home';
-import AboutCoreCarousel from '../Components/AboutCoreCarousel';
 
 const AboutUs: React.FC = () => {
   return (
     <div className='bg-[#0d0d0d]'>
       <Home/>      
-      <AboutCoreCarousel />
       <Faculty/>
 
     </div>
