26583f7a15763b5b4972788fbe12b94381ca6119
teena14 <teena14095@gmail.com>
2025-07-14 00:59:48 +0530

    responsive carousel

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -1,11 +1,13 @@
 import Faculty from '../Components/Faculty';
 import React from 'react';
 import Home from '../Components/Home';
+import AboutCoreCarousel from '../Components/AboutCoreCarousel';
 
 const AboutUs: React.FC = () => {
   return (
     <div className='bg-[#0d0d0d]'>
-      <Home/>      
+      <Home/>  
+      <AboutCoreCarousel/>    
       <Faculty/>
 
     </div>
